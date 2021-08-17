d0c3db3a378a2a35379bc61d2b6b2ecdf2e7f941
Mauro Guida <Jimbus369@gmail.com>
2021-08-17 10:51:58 +0200

    + LoginActivity now Checks and Requests Location permission

--- app/app/src/main/java/com/jdt/locationhub/LoginActivity.java
+++ app/app/src/main/java/com/jdt/locationhub/LoginActivity.java
@@ -1,9 +1,13 @@
 package com.jdt.locationhub;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.lifecycle.ViewModelProviders;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,6 +15,9 @@ import android.widget.EditText;
 import com.jdt.locationhub.viewmodel.LoginViewModel;
 
 public class LoginActivity extends AppCompatActivity {
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 100;
+    private boolean locationPermissionGranted = false;
+
     private LoginViewModel loginViewModel;
 
     private EditText usernameEditT;
@@ -26,12 +33,44 @@ public class LoginActivity extends AppCompatActivity {
         usernameEditT = findViewById(R.id.username_EditT_LoginActivity);
         loginButton = findViewById(R.id.login_Button_LoginActivity);
 
+        //Check and Request Location permission
+        getLocationPermission();
+
         loginButton.setOnClickListener(l -> {
-            if (loginViewModel.login(usernameEditT.getText().toString())) {
+            if (locationPermissionGranted && loginViewModel.login(usernameEditT.getText().toString())) {
                 Intent i = new Intent(this, MainActivity.class);
                 i.putExtra("USERNAME", usernameEditT.getText().toString());
                 startActivity(i);
             }
         });
+    }
+
+    /*
+     * Request location permission.
+     * The result of the permission request is handled
+     * by a callback, onRequestPermissionsResult.
+     */
+    private void getLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationPermissionGranted = true;
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        locationPermissionGranted = false;
+        if (requestCode == PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION) {
+            // If request is cancelled, the result arrays are empty.
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                locationPermissionGranted = true;
+            }
+        }
     }
 }
